ac1980f14b82cf11c78c396d81f0807d8e10022f
Rosario <rosario.cavelli@icloud.com>
2025-04-07 17:07:46 +0200

    Minor changes while porting to the real robot

--- src/base_optimization/src/base_optimization/des_EE_pose_publisher.py
+++ src/base_optimization/src/base_optimization/des_EE_pose_publisher.py
@@ -17,11 +17,11 @@ des_EE_pose.pose.position.x = -3.5
 des_EE_pose.pose.position.y = 2.75
 des_EE_pose.pose.position.z = 0.4
 
-des_orientation = tf.transformations.quaternion_from_euler(0, 0, np.deg2rad(179), axes='sxyz')
+des_orientation = tf.transformations.quaternion_from_euler(0, 0, np.deg2rad(120), axes='sxyz')
 des_EE_pose.pose.orientation.x = des_orientation[0]
 des_EE_pose.pose.orientation.y = des_orientation[1]
 des_EE_pose.pose.orientation.z = des_orientation[2]
-des_EE_pose.pose.orientation.w = np.ma([0.0001, des_orientation[3]])
+des_EE_pose.pose.orientation.w = des_orientation[3]
 
 # create a publisher to publish the desired end-effector pose
 des_EE_topic = rospy.Publisher("/des_EE_pose", PoseStamped, queue_size=10)
